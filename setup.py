2ef7fcc71be1125f1e4cc5686b8000447bc4e11d
Adrian Matejov <a.matejov@centrum.sk>
2014-10-30 15:15:37 +0100

    typos
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='cpsk',
     
-    description='an unofficial api for grabbing data from cp.sk'
+    description='an unofficial api for grabbing data from cp.sk',
     author='Adrian Matejov',
     author_email='a.matejov@centrum.sk',
     
     url='https://github.com/Adman/python-cpsk-api',
     
-    include_package_data=True
-    packages=['cpsk']
+    include_package_data=True,
+    packages=['cpsk'],
 
     license=open('LICENSE').read()
 )

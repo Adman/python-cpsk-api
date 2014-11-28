728ae91ca7d8c6bb6cd78332acc09842dadd7626
Adrian Matejov <a.matejov@centrum.sk>
2014-11-28 14:26:00 +0100

    changed structure -- code moved to __init__
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

---
+++ cpsk/__init__.py
@@ -0,0 +1,90 @@
+import requests
+import datetime
+from lxml import html
+
+CPSK_URL = 'http://cp.atlas.sk/{0}/spojenie/'
+
+class Drive(object):
+    departure = None
+    line_change = None
+    dest = None
+
+    departure_time = None
+    # lc means line change
+    lc_arr_time = None
+    lc_dep_time = None
+    arrival_time = None
+
+    duration = None
+    distance = None
+
+    vehicles = []
+
+    def __repr__(self):
+        if self.line_change is not None:
+            return "[{9}] {0} {1} >> {2} {3} > {4} \
+                    >> {5} {6} ({7}, {8})".format(self.departure,
+                                                  self.departure_time,
+                                                  self.line_change,
+                                                  self.lc_arr_time,
+                                                  self.lc_dep_time,
+                                                  self.dest,
+                                                  self.arrival_time,
+                                                  self.duration,
+                                                  self.distance,
+                                                  self.vehicles[0])
+        else:
+            return "[{6}]{0} {1} >> {2} {3} ({4}, {5})".format(self.departure,
+                                                          self.departure_time,
+                                                          self.dest,
+                                                          self.arrival_time,
+                                                          self.duration,
+                                                          self.distance,
+                                                          self.vehicles[0])
+
+
+def get_routes(departure, dest, vehicle='vlakbus', time='', date=''):
+    if time == '':
+        time = datetime.datetime.now().strftime("%H:%M")
+
+    if date == '':
+        date = datetime.datetime.now().strftime("%d.%m.%Y")
+
+    try:
+        req = requests.get(CPSK_URL.format(vehicle),
+                        params={'date': date, 'time': time, 'f': departure,
+                                't': dest, 'fc': 1, 'tc': 1,
+                                'submit': 'true'})
+    except:
+        return False
+
+    tree = html.fromstring(req.text)
+    html_tables = tree.xpath('//div[@id="main-res-inner"]/table')
+    routes = []
+
+    for table in html_tables:
+        drive = Drive()
+
+        datalen = len(table.xpath('./tr'))
+        if datalen == 4:
+            drive.line_change = table.xpath('./tr[2]/td[3]/text()')
+            drive.lc_arr_time = table.xpath('./tr[2]/td[4]/text()')
+            drive.lc_dep_time = table.xpath('./tr[2]/td[5]/text()')
+
+        drive.departure = table.xpath('./tr[1]/td[3]/text()')
+        drive.dest = table.xpath('./tr[' + str(datalen-1) + ']/td[3]/text()')
+        drive.departure_time = table.xpath('./tr[1]/td[5]/text()')
+        drive.arrival_time = table.xpath('./tr[' + str(datalen-1) + \
+                                            ']/td[4]/text()')
+
+        drive.vehicles.append(table.xpath('./tr[1]/td[7]/img[0]').get('title') \
+                            .replace('Autobus', 'Bus'))
+
+        drive.duration = table.xpath('./tr[' + str(datalen) + \
+                                        ']/td[3]/p/strong[1]/text()')
+        drive.distance = table.xpath('./tr[' + str(datalen) + \
+                                        ']/td[3]/p/strong[2]/text()')
+
+        routes.append(drive)
+
+    return routes

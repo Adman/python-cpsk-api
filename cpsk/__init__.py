a6ed8f741c89a7130611e25d1c7c77b43b7254b0
Adrian Matejov <a.matejov@centrum.sk>
2014-11-28 16:05:58 +0100

    update: Drive class
    * made variables not being global, but just class'
      itself
    * each information is grabbed from a list[0]
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- cpsk/__init__.py
+++ cpsk/__init__.py
@@ -5,42 +5,44 @@ from lxml import html
 CPSK_URL = 'http://cp.atlas.sk/{0}/spojenie/'
 
 class Drive(object):
-    departure = None
-    line_change = None
-    dest = None
+    def __init__(self):
+        self.departure = None
+        self.line_change = None
+        self.dest = None
 
-    departure_time = None
-    # lc means line change
-    lc_arr_time = None
-    lc_dep_time = None
-    arrival_time = None
+        self.departure_time = None
+        # lc means line change
+        self.lc_arr_time = None
+        self.lc_dep_time = None
+        self.arrival_time = None
 
-    duration = None
-    distance = None
+        self.duration = None
+        self.distance = None
 
-    vehicles = []
+        self.vehicles = []
 
     def __repr__(self):
         if self.line_change is not None:
-            return "[{9}] {0} {1} >> {2} {3} > {4} \
-                    >> {5} {6} ({7}, {8})".format(self.departure,
-                                                  self.departure_time,
-                                                  self.line_change,
-                                                  self.lc_arr_time,
-                                                  self.lc_dep_time,
-                                                  self.dest,
-                                                  self.arrival_time,
-                                                  self.duration,
-                                                  self.distance,
-                                                  self.vehicles[0])
+            return "[{0}] {1} {2} >> {3} {4} > {5} >> {6} {7} ({8}, {9})" \
+                                    .format(self.vehicles[0],
+                                            self.departure.encode("utf-8"),
+                                            self.departure_time,
+                                            self.line_change.encode("utf-8"),
+                                            self.lc_arr_time,
+                                            self.lc_dep_time,
+                                            self.dest.encode("utf-8"),
+                                            self.arrival_time,
+                                            self.duration,
+                                            self.distance)
         else:
-            return "[{6}]{0} {1} >> {2} {3} ({4}, {5})".format(self.departure,
-                                                          self.departure_time,
-                                                          self.dest,
-                                                          self.arrival_time,
-                                                          self.duration,
-                                                          self.distance,
-                                                          self.vehicles[0])
+            return "[{0}] {1} {2} >> {3} {4} ({5}, {6})" \
+                                    .format(self.vehicles[0],
+                                            self.departure.encode("utf-8"),
+                                            self.departure_time,
+                                            self.dest.encode("utf-8"),
+                                            self.arrival_time,
+                                            self.duration,
+                                            self.distance)
 
 
 def get_routes(departure, dest, vehicle='vlakbus', time='', date=''):
@@ -59,31 +61,31 @@ def get_routes(departure, dest, vehicle='vlakbus', time='', date=''):
         return False
 
     tree = html.fromstring(req.text)
-    html_tables = tree.xpath('//div[@id="main-res-inner"]/table')
+    html_tables = tree.xpath('//div[@id="main-res-inner"]/table[1]/tbody')
     routes = []
-
+    
     for table in html_tables:
         drive = Drive()
 
         datalen = len(table.xpath('./tr'))
         if datalen == 4:
-            drive.line_change = table.xpath('./tr[2]/td[3]/text()')
-            drive.lc_arr_time = table.xpath('./tr[2]/td[4]/text()')
-            drive.lc_dep_time = table.xpath('./tr[2]/td[5]/text()')
+            drive.line_change = table.xpath('./tr[2]/td[3]/text()')[0]
+            drive.lc_arr_time = table.xpath('./tr[2]/td[4]/text()')[0]
+            drive.lc_dep_time = table.xpath('./tr[2]/td[5]/text()')[0]
 
-        drive.departure = table.xpath('./tr[1]/td[3]/text()')
-        drive.dest = table.xpath('./tr[' + str(datalen-1) + ']/td[3]/text()')
-        drive.departure_time = table.xpath('./tr[1]/td[5]/text()')
+        drive.departure = table.xpath('./tr[1]/td[3]/text()')[0]
+        drive.dest = table.xpath('./tr[' + str(datalen-1) + ']/td[3]/text()')[0]
+        drive.departure_time = table.xpath('./tr[1]/td[5]/text()')[0]
         drive.arrival_time = table.xpath('./tr[' + str(datalen-1) + \
-                                            ']/td[4]/text()')
+                                            ']/td[4]/text()')[0]
 
-        drive.vehicles.append(table.xpath('./tr[1]/td[7]/img[0]').get('title') \
-                            .replace('Autobus', 'Bus'))
+        drive.vehicles.append(table.xpath('./tr[1]/td[7]/img[1]')[0] \
+                                .get('title').replace('Autobus', 'Bus'))
 
         drive.duration = table.xpath('./tr[' + str(datalen) + \
-                                        ']/td[3]/p/strong[1]/text()')
+                                        ']/td[3]/p/strong[1]/text()')[0]
         drive.distance = table.xpath('./tr[' + str(datalen) + \
-                                        ']/td[3]/p/strong[2]/text()')
+                                        ']/td[3]/p/strong[2]/text()')[0]
 
         routes.append(drive)
 

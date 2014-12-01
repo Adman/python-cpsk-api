e94c1b4a67fd14f6caa830a89b2d5cf69b483f00
Adrian Matejov <a.matejov@centrum.sk>
2014-12-01 22:59:56 +0100

    setup: install_requires
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ setup(
     description='an unofficial api for grabbing data from cp.sk',
     author='Adrian Matejov',
     author_email='a.matejov@centrum.sk',
-    version='0.0.3',
+    version='0.0.5',
    
     url='https://github.com/Adman/python-cpsk-api',
-    
+
+    install_requires=['requests'],
     include_package_data=True,
     packages=['cpsk'],
 
-    license=open('./LICENSE').read()
+    license="The MIT License (MIT)"
 )

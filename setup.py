d68805622639fc1f04ddeee0b058932b94c31ae5
Adrian Matejov <a.matejov@centrum.sk>
2014-12-01 18:27:03 +0100

    setup: v0.0.3
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     description='an unofficial api for grabbing data from cp.sk',
     author='Adrian Matejov',
     author_email='a.matejov@centrum.sk',
-    version='0.0.1',
+    version='0.0.3',
    
     url='https://github.com/Adman/python-cpsk-api',
     
     include_package_data=True,
     packages=['cpsk'],
 
-    license=open('LICENSE').read()
+    license=open('./LICENSE').read()
 )

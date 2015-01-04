445b9f90c76b9d32d09e8af4c849b56bd86037b9
Adrian Matejov <a.matejov@centrum.sk>
2015-01-04 15:37:02 +0100

    janitoring: using format to concatenate strings
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- cpsk/__init__.py
+++ cpsk/__init__.py
@@ -32,9 +32,9 @@ class Drive(object):
         self.lines = []
 
     def __repr__(self):
-        return (' >> '.join(map(str, self.lines)) +
-                ' ({0}, {1})'.format(self.duration,
-                                     self.distance)).encode('utf-8')
+        return ('{0} ({1}, {2})'.format(' >> '.join(map(str, self.lines)),
+                                        self.duration,
+                                        self.distance)).encode('utf-8')
 
 
 def get_routes(departure, dest, vehicle='vlakbus', time='', date=''):

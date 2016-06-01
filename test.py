5c5defd5269203e62b8c257558512d843124dd11
Adrian Matejov <a.matejov@centrum.sk>
2016-06-01 21:45:17 +0200

    test: added international lines
    * also the output is now more readable
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- test.py
+++ test.py
@@ -8,13 +8,27 @@ if (sys.version).startswith('2'):
     sys.setdefaultencoding("utf-8")
 
 bus_routes = cpsk.get_routes('TO', 'BA', vehicle='bus')
+print('Looking for bus lines')
 for b in bus_routes:
     print(b)
 
+print()
+
 train_routes = cpsk.get_routes('NR', 'TO', vehicle='vlak')
+print('Looking for train lines')
 for t in train_routes:
     print(t)
 
+print()
+
 routes = cpsk.get_routes('KE', 'TO')
+print('Looking for lines')
 for r in routes:
     print(r)
+
+print()
+
+inter_routes = cpsk.get_routes('Brno', 'BA', vehicle='vlak')
+print('Looking for international lines')
+for ir in inter_routes:
+    print(ir)
